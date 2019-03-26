f2b15dd5ad09acc65c4c0ed9d4b2d5dfedd454b3
JoanDeulofeu <jgehin@student.42.fr>
2019-03-26 16:34:15 +0100

    d1ex04 et 05 ok

--- Day01/ex03/ft_split.php
+++ Day01/ex03/ft_split.php
@@ -1,4 +1,11 @@
 #!/usr/bin/php
 <?PHP
+function ft_split($str)
+{
+	$tab = array_filter(explode(" ", $str));
+	sort($tab);
+	return ($tab);
+}
 
+print_r(ft_split("hello     world     demain matin     Zzz Aaa"))
 ?>

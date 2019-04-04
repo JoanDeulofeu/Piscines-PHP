f97b693e80dfe1c01835dcfb2e38cb7e66de479f
JoanDeulofeu <jgehin@student.42.fr>
2019-04-04 13:52:39 +0200

    d3e6 give up #CaMaSoulé

--- Day03/ex06/members_only.php
+++ Day03/ex06/members_only.php
@@ -1,3 +1,12 @@
 <?PHP
-
+	if ($_SERVER['PHP_AUTH_USER'] == "zaz" && $_SERVER['PHP_AUTH_PW'] == "jaimelespetitsponeys")
+	{
+		echo "lol\n";
+	}
+	else
+	{
+		header("WWW-Authenticate: Basic realm='Espace membres'");
+		header('HTTP/1.0 401 Unauthorized');
+		echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
+	}
 ?>

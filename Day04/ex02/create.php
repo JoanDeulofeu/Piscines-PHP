5682bd429cc0913285ca131b4f95a4566fcd894e
JoanDeulofeu <jgehin@student.42.fr>
2019-04-08 09:09:55 +0200

    Dernier push de la fake piscine. rdv en mai.

--- Day04/ex02/create.php
+++ Day04/ex02/create.php
@@ -5,7 +5,7 @@ $error = "ERROR\n";
 $dejavu = 0;
 if (isset($_POST["submit"]) && isset($_POST["passwd"]) && isset($_POST["login"]))
 {
-	if (file_exists($path) === FALSE)
+	if (file_exists($path) === FALSE) //si dossier existe pas
 		mkdir($path, 0777, true);
 	if ($_POST["submit"] != NULL && $_POST["passwd"] != NULL && $_POST["login"] != NULL)
 	{
@@ -13,7 +13,7 @@ if (isset($_POST["submit"]) && isset($_POST["passwd"]) && isset($_POST["login"])
 		$_SESSION["passwd"] = $_POST["passwd"];
 		$_SESSION["submit"] = $_POST["submit"];
 		$secu = serialize($_SESSION);
-		if (file_exists($passwd) === FALSE)
+		if (file_exists($passwd) === FALSE) //si fichier existe pas
 		{
 			$fichier = fopen($passwd, 'w+');
 			$tab = array($secu);
@@ -22,7 +22,8 @@ if (isset($_POST["submit"]) && isset($_POST["passwd"]) && isset($_POST["login"])
 		else
 		{
 			$tab = file_get_contents($passwd);
-			foreach ($tab as $key => $value) {
+			foreach ($tab as $value)
+			{
 				$value = unserialize($value);
 				if ($value["login"] == $_SESSION["login"])
 				{
@@ -30,6 +31,10 @@ if (isset($_POST["submit"]) && isset($_POST["passwd"]) && isset($_POST["login"])
 					$dejavu = 1;
 				}
 			}
+			if ($dejavu == 1) // si le login existe deja
+				echo $error;
+			else
+				array_push($tab, $secu);
 		}
 	}
 	else

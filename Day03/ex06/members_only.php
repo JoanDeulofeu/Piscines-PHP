496339352a971a2a314228dd165f72265f76d59b
JoanDeulofeu <jgehin@student.42.fr>
2019-05-21 12:01:03 +0200

    day3 correction

--- Day03/ex06/members_only.php
+++ Day03/ex06/members_only.php
@@ -1,12 +1,21 @@
 <?PHP
 	if ($_SERVER['PHP_AUTH_USER'] == "zaz" && $_SERVER['PHP_AUTH_PW'] == "jaimelespetitsponeys")
 	{
-		echo "lol\n";
+		echo "<html><body>\nBonjour Zaz<br />\n";
+		echo "<img src='data:img/42.png;";
+		$image = base64_encode(file_get_contents("../img/42.png"));
+		echo $image;
+		echo "'>\n</body></html>\n";
 	}
 	else
 	{
 		header("WWW-Authenticate: Basic realm='Espace membres'");
 		header('HTTP/1.0 401 Unauthorized');
-		echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
+		$str = "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
+		$len = strlen($str);
+        header("Content-length:$len");
+        header("Connection: close");
+        header("Content-Type: text/html");
+		echo $str;
 	}
 ?>

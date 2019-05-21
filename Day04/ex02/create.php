cbc1d03812440ac2f4238bbb5b7a2b7b41cd4a1f
JoanDeulofeu <jgehin@student.42.fr>
2019-05-21 14:09:20 +0200

    save

--- Day04/ex02/create.php
+++ Day04/ex02/create.php
@@ -5,36 +5,36 @@ $error = "ERROR\n";
 $dejavu = 0;
 if (isset($_POST["submit"]) && isset($_POST["passwd"]) && isset($_POST["login"]))
 {
+	$psw = $_POST["passwd"];
 	if (file_exists($path) === FALSE) //si dossier existe pas
 		mkdir($path, 0777, true);
 	if ($_POST["submit"] != NULL && $_POST["passwd"] != NULL && $_POST["login"] != NULL)
 	{
-		$_SESSION["login"] = $_POST["login"];
-		$_SESSION["passwd"] = $_POST["passwd"];
-		$_SESSION["submit"] = $_POST["submit"];
-		$secu = serialize($_SESSION);
 		if (file_exists($passwd) === FALSE) //si fichier existe pas
 		{
-			$fichier = fopen($passwd, 'w+');
-			$tab = array($secu);
-			fwrite($fichier, $tab);
+			$tab = array('login'=>$_POST['login'], 'passwd'=>$psw, 'submit'=>$_POST['submit']);
+			$secur = serialize(array($tab));
+			file_put_contents($passwd, $secur);
+			echo "OK\n";
 		}
 		else
 		{
 			$tab = file_get_contents($passwd);
-			foreach ($tab as $value)
+			$unsecur = unserialize($tab);
+			foreach ($unsecur as $value)
 			{
-				$value = unserialize($value);
 				if ($value["login"] == $_SESSION["login"])
-				{
-					echo $error;
 					$dejavu = 1;
-				}
 			}
 			if ($dejavu == 1) // si le login existe deja
 				echo $error;
 			else
-				array_push($tab, $secu);
+			{
+				$tab = array('login'=>$_POST['login'], 'passwd'=>$pwd);
+				$secur = serialize(array($tab));
+				file_put_contents($passwd, $secur);
+				echo "OK\n";
+			}
 		}
 	}
 	else
